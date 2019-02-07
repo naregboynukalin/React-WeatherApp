76a07827576dfe7c5e56018661fc09cc86067f3f
naregboynukalin <naregboynukalin@gmail.com>
2019-02-08 02:40:08 +0300

    son durum

--- src/App.js
+++ src/App.js
@@ -3,28 +3,74 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
-const API_KEY="54264c39dc75468daa9bec02dcc552ef";
+const API_KEY="17ff86bff922206ddafa409c651eaec0";
 
 class App extends React.Component{
+    state = {
+        temperature: undefined,
+        city: undefined,
+        country:undefined,
+        humidity:undefined,
+        description:undefined,
+        error:undefined
+    }
 
 
     getWeather= async (e) => {
+        
+       
+        
         e.preventDefault();
-        const api_call = await fetch(`https://api.darksky.net/forecast/&{API_KEY}/37.8267,-122.4233`);
-        const data = await api_call.json();
-        console.log(data);
-    }
-        render(){
-            return (
+        const city =e.target.elements.city.value;
+        const country =e.target.elements.country.value;
 
-               <div> 
-              <Titles />
-                <Form getWeather={this.getWeather}/>
-            <Weather />
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        const data = await api_call.json();
+        if(city&&country){
+            this.setState({
+                temperature: data.main.temp,
+                city: data.name,
+                country:data.sys.country,
+                humidity:data.main.humidity,
+                description:data.weather[0].description,
+                error:""
+            })
+        }else{
+            this.setState({
+            
+            temperature: undefined,
+            city: undefined,
+            country:undefined,
+            humidity:undefined,
+            description:undefined,
+            error:"Please enter the values."
+        });
+    }
+    }
+    
+    render(){
+        return (
+
+           <div> 
+          <Titles />
+            <Form getWeather={this.getWeather}/>
+            <Weather 
+              temperature={this.state.temperature}
+              city={this.state.city}
+              country={this.state.country}
+              humidity={this.state.humidity}
+              description={this.state.description}
+              error={this.state.error}
+
+                
+                
+                />
             </div>
             );
 
-    }
-}
+            }
+};
+
+
 
 export default App;
